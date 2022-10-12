be59837f510d04c1517794007368ed7058bda423
zhaoxiaoyan <m16643548159@163.com>
2022-10-12 14:13:56 +0800

    idea commit

---
+++ modtest/src/teat/a/GitTest.java
@@ -0,0 +1,7 @@
+package teat.a;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ modtest/src/teat/a/TestJava.java
@@ -0,0 +1,8 @@
+package teat.a;
+
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("word");
+    }
+}
