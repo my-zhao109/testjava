787c14a17c01d1155a09e6787dcb169a8df652da
zhaoxiaoyan <m16643548159@163.com>
2022-10-12 19:27:45 +0800

    idea commit

--- modtest/src/teat/a/GitTest.java
+++ modtest/src/teat/a/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test");
-        System.out.println("test");
+        System.out.println("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");
     }
 }
